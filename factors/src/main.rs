de19decd5a35f66ef6f24801a2b38034c3b449a0
Spencer Solit <ssolit@gmail.com>
2023-03-16 14:30:06 -0400

    Completed step 2: renaming things

--- factors/src/main.rs
+++ factors/src/main.rs
@@ -1,15 +1,12 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method is `multiply`, replace `METHOD_NAME_ID` with `MULTIPLY_ID` and replace `METHOD_NAME_PATH` with `MULTIPLY_PATH`
-use methods::{METHOD_NAME_ID, METHOD_NAME_PATH};
+use methods::{MULTIPLY_ELF, MULTIPLY_ID};
 use risc0_zkvm::Prover;
 // use risc0_zkvm::serde::{from_slice, to_vec};
 
 fn main() {
     // Make the prover.
-    let method_code = std::fs::read(METHOD_NAME_PATH)
-        .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
-    let mut prover = Prover::new(&method_code, METHOD_NAME_ID).expect(
-        "Prover should be constructed from valid method source code and corresponding method ID",
-    );
+    let mut prover = Prover::new(MULTIPLY_ELF, MULTIPLY_ID)
+        .expect("Prover should be constructed from valid method source code and corresponding image ID");
 
     // TODO: Implement communication with the guest here
 
@@ -18,9 +15,8 @@ fn main() {
         .expect("Code should be provable unless it 1) had an error or 2) overflowed the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
 
     // Optional: Verify receipt to confirm that recipients will also be able to verify your receipt
-    receipt.verify(METHOD_NAME_ID).expect(
-        "Code you have proven should successfully verify; did you specify the correct method ID?",
-    );
+    receipt.verify(MULTIPLY_ID)
+        .expect("Code you have proven should successfully verify; did you specify the correct image ID?");
 
     // TODO: Implement code for transmitting or serializing the receipt for other parties to verify here
 }
