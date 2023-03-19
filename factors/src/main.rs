2f3688233610f0f2c729833049bdc00f3e894537
Spencer Solit <ssolit@gmail.com>
2023-03-19 15:39:14 -0400

    finished step 3 (share values with guest) and step 4 (multiply vals and commit)

--- factors/src/main.rs
+++ factors/src/main.rs
@@ -1,14 +1,20 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method is `multiply`, replace `METHOD_NAME_ID` with `MULTIPLY_ID` and replace `METHOD_NAME_PATH` with `MULTIPLY_PATH`
 use methods::{MULTIPLY_ELF, MULTIPLY_ID};
 use risc0_zkvm::Prover;
-// use risc0_zkvm::serde::{from_slice, to_vec};
+use risc0_zkvm::serde::{from_slice, to_vec};
 
 fn main() {
+
+    let a: u64 = 17;
+    let b: u64 = 23;
+
     // Make the prover.
     let mut prover = Prover::new(MULTIPLY_ELF, MULTIPLY_ID)
         .expect("Prover should be constructed from valid method source code and corresponding image ID");
 
     // TODO: Implement communication with the guest here
+    prover.add_input_u32_slice(&to_vec(&a).unwrap());
+    prover.add_input_u32_slice(&to_vec(&b).unwrap());
 
     // Run prover & generate receipt
     let receipt = prover.run()
--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,10 +1,16 @@
-// TODO: Rename this file to change the name of this method from METHOD_NAME
-
 #![no_main]
 #![no_std]  // std support is experimental, but you can remove this to try it
 
 risc0_zkvm::guest::entry!(main);
+use risc0_zkvm::guest::env;
 
 pub fn main() {
     // TODO: Implement your guest code here
+    let a: u64 = env::read();
+    let b: u64 = env::read();
+    if a==1 || b==1 {
+        panic!("Trivial Factors");
+    }
+    let product = a.checked_mul(b).expect("Integer overflow");
+    env::commit(&product);
 }
